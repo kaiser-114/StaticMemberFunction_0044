5bb5e10cbf0ca878c4d484a435da4718595e694a
kaiser-114 <areezilla@gmail.com>
2026-05-04 13:35:05 +0700

    Add class definition for buku with chainable setJudul method

--- chainfunctioncall0044.cpp
+++ chainfunctioncall0044.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+class buku{
+    string judul;
+
+    public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this; //chain function call
+    }
+    string getJudul() {
+        return this->judul;
+    }
+
+};
+
+int main()
+{}
