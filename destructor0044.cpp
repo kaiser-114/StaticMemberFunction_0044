e927485a97cd5857cd91d73c2e631c8de1102f76
kaiser-114 <areezilla@gmail.com>
2026-05-04 13:23:09 +0700

    Refactor angka class: move cetakData and isiData methods outside of destructor

--- destructor0044.cpp
+++ destructor0044.cpp
@@ -26,3 +26,24 @@ angka::~angka() {
     delete[] arr;
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << "=" << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main()
+{}
